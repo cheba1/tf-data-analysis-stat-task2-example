824791eebd9bff29e6ae8d4369f630febf5b05be
cheba1 <113043236+cheba1@users.noreply.github.com>
2023-04-03 15:19:38 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,25 +1,28 @@
 import pandas as pd
 import numpy as np
-
-from scipy.stats import norm
+from scipy.stats import chi2
 
 
 chat_id = 725861714 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     n = len(x)
-    loc = np.mean(x)
-    var = np.var(x)
-    # Находим квантили хи-квадрат распределения
     alpha = 1 - p
-    chi2_alpha_2 = chi2.ppf(alpha / 2, n - 1)
-    chi2_1_alpha_2 = chi2.ppf(1 - alpha / 2, n - 1)
     
-    # Вычисляем границы доверительного интервала
-    L = (n - 1) * var / chi2_1_alpha_2
-    R = (n - 1) * var / chi2_alpha_2
+    # Шаг 2
+    x_mean = x.mean()
+    s2 = np.var(x, ddof=1)
     
-    return L, R
+    # Шаг 3
+    w = (n - 1) * s2 / 18**2
+    
+    # Шаг 4
+    chi2_left = chi2.ppf(alpha/2, df=n-1)
+    chi2_right = chi2.ppf(1-alpha/2, df=n-1)
+    
+    # Шаг 5
+    sigma_left = np.sqrt((n - 1) * s2 / chi2_right)
+    sigma_right = np.sqrt((n - 1) * s2 / chi2_left)
+    
+    # Шаг 7
+    return (sigma_left, sigma_right)
