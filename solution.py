001b574334c789ea20c0531da61ff1fe8909b02e
cheba1 <113043236+cheba1@users.noreply.github.com>
2023-04-05 15:57:55 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, chi2
 
 
 chat_id = 725861714 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Вычисляем выборочное среднее и стандартное отклонение
-    x_mean = np.mean(x)
-    x_std = np.std(x, ddof=1)
-
-    # Вычисляем левую и правую границы доверительного интервала
-    n = len(x)
     alpha = 1 - p
-    z = x_std / np.sqrt(n)
-    left = x_mean - z * norm.ppf(1 - alpha / 2)
-    right = x_mean - z * norm.ppf(alpha / 2)
-
-    return left, right
+    n = len(x)
+    
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+    
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2) #b
+    right = chi2_rv.ppf(alpha / 2) #a
+    
+    return np.sqrt(n * x2_mean / (left * 48)), np.sqrt(n * x2_mean / (right * 48))
